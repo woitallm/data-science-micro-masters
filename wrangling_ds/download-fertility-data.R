389bd8230334d7e03f3370f0e161e9c53e14a789
woitallm <woit0014@d.umn.edu>
2018-11-30 12:09:12 -0600

    Updated R files to download the fertility data from dslabs and another to start wrangling (incomplete)

--- wrangling_ds/download-fertility-data.R
+++ wrangling_ds/download-fertility-data.R
@@ -1,10 +1,8 @@
-rm(list = ls())
 library(tidyverse)
+try(setwd("./wrangling_ds"),silent = T)
 
 path<- system.file("extdata",package = "dslabs")
 filename<-file.path(path,"fertility-two-countries-example.csv")
 file.copy(filename,"./data")
 
-fertility<-read_csv("./data/fertility-two-countries-example.csv")
-
-rm(path, filename)
+rm(list = ls())
---
+++ wrangling_ds/wrangle-fertility-data.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+try(setwd("./wrangling_ds"),silent = T)
+
+fertility<-read_csv("./data/fertility-two-countries-example.csv")
